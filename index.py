0b4ada61d615557b56326ad95dff3fe65df423ff
Passapol <phasphlsuthahthrrm976@gmail.com>
2025-10-20 02:01:34 +0700

    test

--- index.py
+++ index.py
@@ -1,18 +1,21 @@
 import pyautogui as at
 import time
 print("เริ่มทำงานใน 5 วินาที...")
-time.sleep(5)
+time.sleep(3)
 
-repeat = 1
+repeat = 20
 
 for i in range(repeat):
+    at.press('tab')
+    at.press('tab')
+    at.press('enter')
+    at.press('down')
+    at.press('down')
+    at.press('down')
+    at.press('enter')
+    at.press('enter')
     at.click()
     at.click()
     time.sleep(1)
-    at.press('up')
-    time.sleep(2)
-    at.press('enter')
-    time.sleep(2)
-    at.press('enter')
 
 print("ทำงานเสร็จแล้ว ✅")
